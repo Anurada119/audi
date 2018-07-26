020e13c2e2655365ec985a35765b6c470c96d0c9
Anurada Gunarathna <anurada119@gmail.com>
2018-07-27 00:20:38 +0530

    home page done

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,38 @@
 $(document).ready(function () {
+
+    searchBox();
+    mobileMenu();
+
     $('.ab-slider').slick({
         autoplay: true,
         arrows: false,
         dots: true,
         infinite: true,
         speed: 1000,
-        fade: true,
+        fade: false,
         cssEase: 'linear'
     });
 });
+
+var searchBox = function(){
+    $('.search-btn').on('click', function(){
+        $(this).siblings('.searchbar').toggleClass('searchbar-open');
+    });
+}
+
+var mobileMenu = function(){
+    $('.mobile-menu-icon').on('click', function(){
+        $(this).toggleClass('open');
+        $('.ab-side-nav').toggleClass('nav-open');
+
+        if($('.ab-side-nav').hasClass('nav-open')){
+            $('body').css({
+                'overflow':'hidden'
+            });
+        }else{
+            $('body').css({
+                'overflow':'auto'
+            });
+        }
+    })
+}
